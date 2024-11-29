341d3ffa8e0aae100eea448bd7156a1105c173b9
BearDad <drakomaster23@gmail.com>
2024-11-29 21:21:04 +0100

    TWEAK

---
+++ jsnotes/Foundations-course/asignments/variables.js
@@ -0,0 +1,22 @@
+// let admin;
+// let name = "John";
+// admin = name;
+// alert(admin);
+//
+// let homePlanet = "Earth";
+// let currentUserName;
+
+// let x = 10;
+// x--;
+// console.log(x);
+//
+
+// const myInt = 10;
+// const myFloat = 10.5;
+//
+// console.log(typeof myInt);
+// console.log(typeof myFloat);
+//
+// let num2 = 6;
+// num2--;
+// console.log(num2);
